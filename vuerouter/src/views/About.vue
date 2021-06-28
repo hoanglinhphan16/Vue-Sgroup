ec00e92870ef5a2303e6f2e582b08b5218abee47
Hoang Linh <hoanglinhphan16>
2021-06-28 20:44:57 +0700

    update

--- vuerouter/src/views/About.vue
+++ vuerouter/src/views/About.vue
@@ -5,14 +5,19 @@
       <p class="content">Future of your life</p>
     </div>
     <div class="background container">
-      <p class="title">We tell stories that drivers the heart</p>
+      <p class="title-header">We tell stories that drivers the heart</p>
       <p class="content-header">
         Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui
         dolor cillum fugiat ad. Id sit mollit labore sunt sit culpa qui minim
         pariatur et officia elit id. Tempor cupidatat veniam esse ad veniam
         dolore excepteur tempor dolor consectetur ut id.
       </p>
+      <span class="line"></span>
       <img src="../assets/angel.jpeg" class="angel" />
+      <img src="../assets/alone.jpg" class="alone">
+      <p class="title-middle">We tell the news that makes the most impact.</p>
+      <p class="content-middle">Laboris consectetur sunt nulla eiusmod voluptate eiusmod dolor nisi qui dolor cillum fugiat ad. Id sit mollit labore sunt sit culpa qui minim pariatur et officia elit id. Tempor cupidatat veniam esse ad veniam dolore excepteur tempor dolor consectetur ut id.
+</p>
     </div>
   </div>
 </template>
@@ -24,9 +29,25 @@ export default {};
 <style scoped>
 .background {
   width: 1245px;
-  height: 2108px;
+  height: 1100px;
 
   background: #1c1c1c;
+}
+.line {
+  position: absolute;
+  width: 8px;
+  height: 1016px;
+  right: 600px;
+  top: 365px;
+
+background: #FFFFFF;
+}
+
+.alone {
+  width: 500px;
+  position: absolute;
+  right: 700px;
+  top: 750px;
 }
 
 img {
@@ -46,7 +67,7 @@ img {
   color: #000000;
 }
 
-.title {
+.title-header {
   position: absolute;
   width: 444px;
   height: 103px;
@@ -61,6 +82,23 @@ img {
   line-height: 52px;
 
   color: #ffffff;
+}
+
+.title-middle {
+  position: absolute;
+  width: 524px;
+  height: 103px;
+  left: 720px;
+  top: 1080px;
+
+  font-family: Libre Baskerville;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 42px;
+  line-height: 52px;
+  text-align: left;
+
+  color: #FFFFFF;
 }
 
 .angel {
@@ -86,5 +124,24 @@ img {
   /* or 150% */
 
   color: #ffffff;
+}
+
+.content-middle {
+  position: absolute;
+  width: 524px;
+  height: 97px;
+  left: 720px;
+  top: 1180px;
+  text-align: left;
+
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 24px;
+  /* or 150% */
+
+
+color: #FFFFFF;
 }
 </style>
